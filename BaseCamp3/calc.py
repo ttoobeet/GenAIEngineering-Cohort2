13ad51bddf2191e5e04b2d16b0ebc518a59b1d65
ttoobeet <ttoobeet@gmail.com>
2025-06-22 19:03:41 +0530

    21-Jun Check In

---
+++ BaseCamp3/calc.py
@@ -0,0 +1,12 @@
+import sys
+print("Hello,Outskillers")
+'''def add(a,b):
+    print(a+b)
+
+add(2,3)'''
+
+if __name__=="__main__":
+    num1=float(sys.argv[1])
+    num2=float(sys.argv[2])
+
+print(num1,num2)
